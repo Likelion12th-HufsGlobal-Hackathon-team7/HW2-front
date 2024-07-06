f5bf70007928cf76037df981019be1e5530ac935
norangsuji <sujihong93@gmail.com>
2024-07-06 09:26:25 +0900

    라이브러리추가 및 app라우트 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from './pages/login/login';
+import RegisterPage from './pages/register/Register';
+import MainPage from './pages/main/Main';
+import InFolderPage from './pages/infolder/InFolder';
 import { NewMemo } from './pages/infolder/NewMemo';
 import { ViewMemo } from './pages/infolder/ViewMemo';
 import { ReMemo } from './pages/infolder/ReMemo';
@@ -6,11 +11,16 @@ import { FolderTitle } from './pages/main/FolderTitle';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/main/infolder" element={< InFolderPage/>} />
+      </Routes>
+    </Router>
   )
-    
 }
 
 export default App;
